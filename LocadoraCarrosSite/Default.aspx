e4eb541ff44fd56e77d116550005be55e1974423
gapessoa <gabriel@controlnet.com.br>
2013-11-20 18:17:12 -0200

    Master Page Formated

--- LocadoraCarrosSite/Default.aspx
+++ LocadoraCarrosSite/Default.aspx
@@ -1,3 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/theMaster.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="LocadoraCarrosSite.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" runat="server">
+    <div class="page-header ">
+        <h1>Home</h1>
+    </div>
+</asp:Content>
--- LocadoraCarrosSite/Master/theMaster.Master.designer.cs
+++ LocadoraCarrosSite/Master/theMaster.Master.designer.cs
@@ -20,5 +20,14 @@ namespace LocadoraCarrosSite.Master {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// cpMainContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cpMainContent;
     }
 }
